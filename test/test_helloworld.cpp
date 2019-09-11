1f2d41ff19a49802e092b07c673f9bf0bd011f6f
nawhi <25394965+nawhi@users.noreply.github.com>
2019-09-11 12:07:59 +0100

    Configure simple first test

--- test/test_helloworld.cpp
+++ test/test_helloworld.cpp
@@ -1,11 +1,11 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest.h>
+#include <doctest/doctest.h>
 
 #include "helloworld.h"
 
 TEST_CASE("checking Hello World")
 {
     HelloWorld hello{"Good Night Vienna"};
-    CHECK( hello.getMessage() != "Good Night Vienna");
+    CHECK( hello.getMessage() == "Good Night Vienna");
 }
