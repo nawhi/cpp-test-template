23ee83e61e292c8360dcac2e81b44ab102d73f08
nawhi <25394965+nawhi@users.noreply.github.com>
2019-09-17 10:41:23 +0100

    Add second test to check linking of multiple suites

--- test/test_helloworld_2.cpp
+++ test/test_helloworld_2.cpp
@@ -2,9 +2,9 @@
 
 #include "helloworld.h"
 
-BOOST_AUTO_TEST_CASE( Hello_World )
+BOOST_AUTO_TEST_CASE( Hello_World_2 )
 {
     HelloWorld hello{"Good Night Vienna"};
 
-    BOOST_CHECK( hello.getMessage() == "Good Night Vienna");
+    BOOST_REQUIRE_EQUAL( hello.getMessage(), "foo");
 }
